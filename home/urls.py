a622f6a029e5a9115f1d132cb0a5af912a3d61b0
le.huynh.duc <SUN-ASTERISK\le.huynh.duc@b122011-lt.sun-asterisk.com>
2020-04-24 14:02:02 +0700

    done user

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+from .views import RegisterView
 
 urlpatterns = [
-   path('', views.index)
+    path('', views.index, name = "home"),
+    path('book/<int:id>/', views.book_detail, name = "book_detail"),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name="pages/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,42 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
-from django.shortcuts import render
+
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+
+from .form import RegisterForm
 from .models import Book
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
 
 
 # Create your views here.
 def index(request):
     data = {'Books': Book.objects.all()[:15], 'Books_Best': Book.objects.all()[:5]}
     return render(request, 'pages/index.html', data)
+
+
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        raise Http404("Not Exist")
+    return render(request, 'pages/register.html', {'book': book})
+
+
+class RegisterView(View):
+    template_name = 'pages/register.html'
+    form_class = RegisterForm
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        register_form = self.form_class(request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            login(request, user)
+
+            return redirect('home')
+        return render(request, self.template_name, {'errors': register_form.errors})
 # Create your views here.
